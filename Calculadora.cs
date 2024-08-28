5a213c04a32ab5ca74772dd9dcb9e92dbca51f52
RaissaGimenes <raissagimenes@rede.ulbra.br>
2024-08-27 21:48:47 -0300

    calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,19 @@
+class Calculadora 
+{
+    public double Soma(double x, double y)
+    {
+        double resultado = x + y;
+        return resultado;
+    }
+    public double Subtracao(double x, double y){
+        return x - y;
+    }
+    public double Multiplicar(double x, double y)
+    {
+        return x * y;
+    }
+    public double Divisão (double x, double y)
+    {
+        return x / y;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Calculadora calc = new Calculadora();
+Console.WriteLine("Informe valor de x:");
+double x = Convert.ToDouble (Console.ReadLine());
+Console.WriteLine("Informe o valor de y:");
+double y = Convert.ToDouble(Console.ReadLine());
+
+ double resultadoSoma = calc.Soma(x,y);
+ Console.WriteLine ($"O resultado da somar de {x} e {y} é igual a : {resultadoSoma}");
+
+ Console.WriteLine($"O resultado da subtrair é: {calc.Subtracao(x,y)}");
+
+ Console.WriteLine($"O resultado da Multiplicação é: {calc.Multiplicar(x,y)}");
+
+ Console.WriteLine($"O resultado da Divisão é: {calc.Divisão(x,y)}");
